801d1ef3e9a35d2104b8bc181afd195eb21e84af
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-08 03:30:32 +0200

    Update __init__.py

--- components/ili9881c/display/__init__.py
+++ components/ili9881c/display/__init__.py
@@ -21,11 +21,21 @@ CONF_PIXEL_FORMAT = "pixel_format"
 CONF_BACKLIGHT_PIN = "backlight_pin"
 CONF_INIT_SEQUENCE = "init_sequence"
 CONF_DELAY = "delay"
+CONF_ROTATION = "rotation"
 
 DEPENDENCIES = ["esp32"]
 
 ili9881c_ns = cg.esphome_ns.namespace("ili9881c")
 ILI9881CDisplay = ili9881c_ns.class_("ILI9881CDisplay", cg.PollingComponent, display.DisplayBuffer)
+
+# Énumérations pour la rotation
+Rotation = ili9881c_ns.enum("Rotation")
+ROTATIONS = {
+    0: Rotation.ROTATION_0,
+    90: Rotation.ROTATION_90,
+    180: Rotation.ROTATION_180,
+    270: Rotation.ROTATION_270,
+}
 
 MODELS = {
     "custom": {
@@ -53,7 +63,6 @@ def validate_init_sequence(value):
         if isinstance(item, list):
             if len(item) < 1:
                 raise cv.Invalid("Command must have at least one byte")
-            # Convertir tous les éléments en entiers
             validated.append([int(x) for x in item])
         elif isinstance(item, dict):
             if CONF_DELAY in item:
@@ -83,6 +92,7 @@ CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend(
         cv.Optional(CONF_BACKLIGHT_PIN): pins.gpio_output_pin_schema,
         cv.Optional(CONF_INVERT_COLORS, default=False): cv.boolean,
         cv.Optional(CONF_AUTO_CLEAR_ENABLED, default=True): cv.boolean,
+        cv.Optional(CONF_ROTATION, default=0): cv.enum(ROTATIONS, int=True),
         cv.Optional(CONF_INIT_SEQUENCE): validate_init_sequence,
         cv.Optional(CONF_DIMENSIONS): cv.Schema(
             {
@@ -98,7 +108,7 @@ CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend(
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    # Seulement register_display, pas les deux !
     await display.register_display(var, config)
 
     model = config[CONF_MODEL]
@@ -125,21 +135,20 @@ async def to_code(config):
     cg.add(var.set_pixel_format(config[CONF_PIXEL_FORMAT]))
     cg.add(var.set_invert_colors(config[CONF_INVERT_COLORS]))
     cg.add(var.set_auto_clear_enabled(config[CONF_AUTO_CLEAR_ENABLED]))
+    cg.add(var.set_rotation(config[CONF_ROTATION]))
 
     # Gestion de la séquence d'initialisation
     if CONF_INIT_SEQUENCE in config:
         init_seq = config[CONF_INIT_SEQUENCE]
         
-        # Convertir la séquence en code C++
         cg.add(var.clear_init_sequence())
         
         for item in init_seq:
             if isinstance(item, list):
-                # C'est une commande
                 cmd = item[0]
                 data = item[1:] if len(item) > 1 else []
                 cg.add(var.add_init_command(cmd, data))
             elif isinstance(item, dict) and CONF_DELAY in item:
-                # C'est un délai
                 delay_ms = item[CONF_DELAY]
                 cg.add(var.add_init_delay(delay_ms))
+
